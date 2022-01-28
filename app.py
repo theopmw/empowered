20730623e220948e3cfbcf8f8802c95b0238f046
Iuliia_Konovalova <78818359+IuliiaKonovalova@users.noreply.github.com>
2022-01-29 01:08:36 +0800

    Juliia (#37)
    * Add emojis images for the feelings scale
    
    * Add markdown for the blog content

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
