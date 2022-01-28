f2ee1fa455bee9f21873d6af35640f527d0cc7f0
theopmw <theowrightdev@gmail.com>
2022-01-28 16:42:36 +0000

    Link DB and test on home template

--- app.py
+++ app.py
@@ -3,17 +3,26 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 
+from flask_pymongo import PyMongo
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    users = mongo.db.users.find()
+    return render_template("home.html", users=users)
 
 
 if __name__ == "__main__":
