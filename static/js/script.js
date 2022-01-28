31488bc9e452fe7538a137e52a2008f507a9b238
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-28 15:22:12 +0800

    Add ids for the links

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,20 @@ $(document).ready(function () {
     $('.nav__options').toggle('.open');
   });
   $('#hamburger').click(function () {
-    $('.navbar__components--mobile').toggle('.open');
+    $('#mobile-menu').toggle('.open-menu');
+  });
+  $(window).on('resize', function () {
+    var win = $(this);
+    if (win.width() >= 1000) {
+      var someElements = document.querySelectorAll('.navbar__components--mobile ');
+      // $('#mobile-menu').toggle('.open-menu')
+      $(someElements[0]).removeClass('.open-menu')
+    }
+
+    // if ($('#mobile-menu').classList.hasClass('open-menu')) {
+    //   $('#mobile-menu').classList.removeClass('open-menu');
+    // };
+    // $('#mobile-menu').attr("class", 'open-menu')
+
   });
 })
